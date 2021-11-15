b30dfa97d34fc28225ba247c384633a1e09742cb
Guilherme Gama <guilherme.s.gam@gmail.com>
2021-11-15 23:52:28 +0000

    Added sound

--- Assets/Scenes/Intro/Scripts/IntroManager.cs
+++ Assets/Scenes/Intro/Scripts/IntroManager.cs
@@ -9,15 +9,30 @@ using UnityEngine.SceneManagement;
 public class IntroManager : MonoBehaviour
 {
     public SceneTransition sceneTransition;
-    public double spaceKeyDuration=2.5f;
+    public double spaceKeyDuration = 2.5f;
     public DateTime? spacePressTime;
     public Image circle;
     public Image fade;
+    public Text spaceHintText;
 
+    bool textIsOut;
     double spaceKeyTimeDif;
+
+    private void Awake()
+    {
+        spaceHintText.color = new Color(255, 255, 255, 0);
+        circle.fillAmount = 0;
+
+    }
     void Start()
     {
-        circle.fillAmount = 0;
+        StartCoroutine(ShowSpaceHint());
+    }
+
+    IEnumerator ShowSpaceHint()
+    {
+        yield return new WaitForSeconds(10);
+        spaceHintText.DOFade(1, 2f).SetEase(Ease.InBack);
     }
 
     // Update is called once per frame
@@ -29,11 +44,19 @@ public class IntroManager : MonoBehaviour
     private void SpacebarHandle()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             spacePressTime = DateTime.Now;
+            if (!textIsOut)
+            {
+                textIsOut = true;
+                spaceHintText.DOFade(0, 1f).SetEase(Ease.InBack);
+            }
+
+        }
 
         if (Input.GetKey(KeyCode.Space) && spacePressTime.HasValue)
         {
-            spaceKeyTimeDif = (DateTime.Now- spacePressTime).Value.TotalSeconds;
+            spaceKeyTimeDif = (DateTime.Now - spacePressTime).Value.TotalSeconds;
             float donePerc = (float)(spaceKeyTimeDif / spaceKeyDuration);
             Debug.Log($"donePerc {donePerc}");
             circle.fillAmount = donePerc;
@@ -47,14 +70,14 @@ public class IntroManager : MonoBehaviour
         }
         else
         {
-            circle.fillAmount =0;
+            circle.fillAmount = 0;
         }
     }
 
     private void SpacebarLongPress()
     {
         Debug.Log($"Long Press Space");
-        fade.DOFade(1, .5f).SetEase(Ease.InBack).onComplete += ()=> sceneTransition.NextScene();
+        fade.DOFade(1, .5f).SetEase(Ease.InBack).onComplete += () => sceneTransition.NextScene();
     }
 
 }
